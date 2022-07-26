81a89d3e36a0bbef0f8a762a0fef41f69273d916
Tatjana Jakona <tatjjako@rix.local>
2022-07-26 14:41:33 +0300

    Initial commit

---
+++ app/src/main/java/com/example/rcs_2607_calculator/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.rcs_2607_calculator;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
